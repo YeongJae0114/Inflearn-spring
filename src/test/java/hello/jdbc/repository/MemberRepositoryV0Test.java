652bc55828e79be31cbd7c1171cca2ee4fb4fd61
YeongJae0114 <yjl8628@naver.com>
2024-02-24 17:41:58 +0900

    jdbc의 이해

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,39 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV3", 10000);
+        repositoryV0.save(member);
+
+        // findById
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        // update
+        repositoryV0.update(member.getMemberId(),20000);
+        Member updateMember = repositoryV0.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repositoryV0.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(()->repositoryV0.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+
+}
