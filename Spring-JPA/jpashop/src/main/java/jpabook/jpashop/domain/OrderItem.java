4ad4ea387a4452a226c65eaefda0e089def3f6b4
YeongJae0114 <yjl8628@naver.com>
2024-04-01 17:41:22 +0900

    🐛 CreateOrderItem 매개변수 수정
    추가로 생성자 제한

--- Spring-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Spring-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter @Setter
 @Table(name = "order_item")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -27,16 +30,14 @@ public class OrderItem {
     private int count;
 
     //== 생성 메서드==//
-    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
-        Order order = new Order();
-        order.setMember(member);
-        order.setDelivery(delivery);
-        for (OrderItem orderItem : orderItems) {
-           order.addOrderItem(orderItem);
-        }
-        order.setStatus(OrderStatus.ORDER);
-        order.setOrderDate(LocalDateTime.now());
-        return order;
+    public static OrderItem createOrderItem( Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStoke(count);
+
+        return orderItem;
     }
 
     //==비즈니스 로직==//
