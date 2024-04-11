f5512c67770bc275541c3edf85f3e52fa210ac1a
YeongJae0114 <yjl8628@naver.com>
2024-04-11 09:42:52 +0900

    String 클래스 기본

---
+++ Java-mid1/java-mid1/src/lang/string/CharArrayMain.java
@@ -0,0 +1,15 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char [] charArr =new char[]{'h','e','l','l','o'};
+        String string = "hello";
+
+        System.out.println(charArr);
+
+        System.out.println(string);
+    }
+
+
+
+}
---
+++ Java-mid1/java-mid1/src/lang/string/StringConcatMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class StringConcatMain {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "java";
+
+        System.out.println(str1.concat(str2));
+        System.out.println(str1+str2);
+    }
+}
