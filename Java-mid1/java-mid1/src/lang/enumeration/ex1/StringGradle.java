b94acd9e49ceb28e890ef333c2edaa545968a839
YeongJae0114 <yjl8628@naver.com>
2024-04-15 15:15:26 +0900

    문자열과 타입 안전성2

---
+++ Java-mid1/java-mid1/src/lang/enumeration/ex1/StringGradle.java
@@ -0,0 +1,8 @@
+package lang.enumeration.ex1;
+
+public class StringGradle {
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+
+}
