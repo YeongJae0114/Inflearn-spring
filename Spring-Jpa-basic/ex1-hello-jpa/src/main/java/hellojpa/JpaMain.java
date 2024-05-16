b310bb3128298d71a3f52ba0cbc19a644b36f446
YeongJae0114 <yjl8628@naver.com>
2024-05-16 10:31:26 +0900

    Hello JPA

--- Spring-Jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Spring-Jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,36 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜잭션 시작
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("Hello");
+            em.persist(member); //Member 객체를 영속 상태로 만든 상태, db 저장은 X
 
-        em.close();
+//          Member findMember = em.find(Member.class, 1L);
+//          findMember.setName("JPA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1.getName());
+            }
+
+            tx.commit(); //db에 저장
+        }catch (Exception E){
+            tx.rollback(); //예외가 발생하면 롤백
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
