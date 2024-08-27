4791cb5a1d3279895f69729b464471cdcc84f669
YeongJae0114 <yjl8628@naver.com>
2024-08-27 15:43:05 +0900

    sec 11

--- Spring-advanced/aop/src/test/java/hello/aop/AopTest.java
+++ Spring-advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,22 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Slf4j
+//@Import(AspectV3.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
     @Autowired
     OrderService orderService;
